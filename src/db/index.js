40404124e0182eeb7eb2dcaad735f3f79082db9f
Ankit4399 <ankitkumar380780@gmail.com>
2025-03-13 15:06:09 +0530

    connecting db

--- src/db/index.js
+++ src/db/index.js
@@ -13,3 +13,23 @@ const connectdb = async ()=>{
 };
 
 export default connectdb;
+
+
+
+/*
+ 1. When connecting to databases, handling potential data-not-found scenarios is essential. Employ try/catch blocks or promises to manage errors or we can also use promises.
+
+        - key to remember : ( wrap in try-catch )
+
+    2. Database operations involve latency, and traditional synchronous code can lead to blocking, where the program waits for the database query to complete before moving on. So, we should async/await which allows for non-blocking execution, enabling the program to continue with other tasks while waiting for the database response. 
+
+        - key to remember :  ( always remember the database is in another continent, so use async await)
+
+    3. env file mei change karte hi, server ko restart karna hi padega, no other option, nodemon env files ka track nahi rakhta.
+    
+- Used two approach to connect the database - 1. In Index File, 2. In Seprate DB file
+
+- Assignments - 
+    - console log `connectionInstance`
+    - Read more about process.exit code
+*/
